8b193596df20ede455a195c8230fa3804a231bde
Rami <39981602+ramizob3a@users.noreply.github.com>
2021-12-18 01:13:16 +0200

    Update callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -169,7 +169,7 @@ def updateCallback(client, callback_query,redis):
     if date[0] == "dlf":
       File = date[1]
       os.system("rm ./files/"+File)
-      url = "https://raw.githubusercontent.com/TshAkEAb/TshakeV2-files/master/"+File
+      url = "https://raw.githubusercontent.com/ramizob3a/TshakeV2-files/master/"+File
       out = requests.get(url).text
       f = open("./files/"+File,"w+")
       f.write(out)
