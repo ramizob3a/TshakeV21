d50a4a7d11db708a6ac7a440d1c882cdcbe68930
Rami <39981602+ramizob3a@users.noreply.github.com>
2021-12-18 10:16:58 +0200

    Update locks.py

--- utlis/locks.py
+++ utlis/locks.py
@@ -281,7 +281,7 @@ def GPck(c,m,redis):
     f = open("./handlers/"+fi,"r") 
     y = f.read() 
     f.close()
-    out = re.findall("zx_xx",y)
+    out = re.findall("rambo_syr",y)
     outJ = re.findall("Ckuser",y)
     iN += len(out)
     iJ += len(outJ)
